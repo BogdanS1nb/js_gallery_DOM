7d43b778a6269b9e4ca522bc2fe0fb0e093a9285
Bogdan Kalytovskiy <bogdankalytovskkiy@gmail.com>
2025-05-10 12:14:31 +0300

    make build

---
+++ index.a4ba32d3.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".gallery__list"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){var n=e.target.closest("a");if(n){e.preventDefault();var r=n.href;t.src=r}})});
+//# sourceMappingURL=index.a4ba32d3.js.map
