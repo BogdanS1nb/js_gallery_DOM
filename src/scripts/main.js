1a57583050adb4b19f00d6bc1c828b8ed3c869e9
Bogdan Kalytovskiy <bogdankalytovskkiy@gmail.com>
2025-05-10 12:09:36 +0300

    add sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const galaryList = document.querySelector('.gallery__list');
+  const largeImg = document.getElementById('largeImg');
+
+  galaryList.addEventListener('click', (e) => {
+    const link = e.target.closest('a');
+
+    if (!link) {
+      return;
+    }
+
+    e.preventDefault();
+
+    const newSrc = link.href;
+
+    largeImg.src = newSrc;
+  });
+});
